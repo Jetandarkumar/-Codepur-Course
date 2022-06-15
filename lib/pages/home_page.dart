c45165fce018db8d9956b30683b2cde82de9723a
Jetandar Kumar <godson0343@gmail.com>
2022-06-15 15:06:50 +0500

    Completed Chapter 11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,26 @@
+import 'package:first/models/catalog.dart';
 import 'package:first/widgets/drawer.dart';
+import 'package:first/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    const int days = 20;
-    const String name = "Codepur";
+  final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: Center(
-          child: Container(
-              child: const Text("Welcome to $days days of code with, $name"))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummyList[index]);
+          },
+        ),
+      ),
       drawer: MyDrawer(),
     );
   }
